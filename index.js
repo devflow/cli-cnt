c5aa191cc155bf48875b656f4e47fea9c6d4c4f2
KyeSeong Ahn <devflow@shelter.id>
2020-08-13 13:57:09 +0900

    ready for deploy

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/what', (req, res) => {
-  res.send(`<html><head><title>CC</title></head><h1>${io.clients.length}</h1></html>`);
+var currentCounter = 0;
+
+app.get('/', (req, res) => {
+  res.send(`<html><head><title>CC</title></head><h1>${currentCounter}</h1></html>`);
 });
 
-http.listen(6974, () => {
-  console.log('listening on *:6974');
+app.all("*", (req, res) => {
+    res.sendStatus(404);
+})
+
+io.on('connect', (soc) => {
+    currentCounter++;
+
+    soc.on('disconnect', () => {
+        currentCounter--;
+    })
+});
+
+http.listen(80, () => {
+  console.log('listening on *:80');
 });
