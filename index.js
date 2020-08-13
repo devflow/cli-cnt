4c3d9d1b105641e21bbd4e656ef524746ece68cb
KyeSeong Ahn <devflow@shelter.id>
2020-08-13 17:00:58 +0900

    add some logging system

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 var currentCounter = 0;
+var logging = [];
 
 app.get('/', (req, res) => {
-  res.send(`<html><head><title>CC</title></head><h1>${currentCounter}</h1></html>`);
+  var logHtml = '';
+
+  logging.forEach((v) => {
+    logHtml += `<li>${v}</li>`;
+  })
+
+  res.send(`<html><head><title>CC</title></head><h1>${currentCounter}</h1><ul>${logHtml}</ul></html>`);
 });
 
 app.all("*", (req, res) => {
@@ -15,8 +22,43 @@ app.all("*", (req, res) => {
 io.on('connect', (soc) => {
     currentCounter++;
 
+    soc['uid'] = "unknown";
+
+    soc.on('start', (data) => {
+      try {
+        soc['uid'] = data.uid;
+      }catch(e){}
+
+      logging.unshift(`${(new Date()).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})}: <b>[----------]</b> ${JSON.stringify(data)}`);
+
+      if (logging.length > 1000) {
+        logging.pop();
+      }
+    });
+
     soc.on('disconnect', () => {
         currentCounter--;
+        logging.unshift(`${(new Date()).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})}: <b>[${soc['uid']}]</b> DISCONNECTED`);
+
+        if (logging.length > 1000) {
+          logging.pop();
+        }
+    });
+
+    soc.on('fcm-error', (data) => {
+      logging.unshift(`${(new Date()).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})}: <b>[${soc['uid']}]</b> FCM ERROR <p style="color:red">${data}</p>`);
+
+      if (logging.length > 1000) {
+        logging.pop();
+      }
+    })
+
+    soc.on('fcm-token', (data) => {
+      logging.unshift(`${(new Date()).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})}: <b>[${soc['uid']}]</b> <p style="color:blue">OK: ${data}</p>`);
+
+      if (logging.length > 1000) {
+        logging.pop();
+      }
     })
 });
 
