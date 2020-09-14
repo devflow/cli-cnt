8176ccf8e730fcba211420e1a41298e9861aaf98
KyeSeong Ahn <devflow@shelter.id>
2020-09-14 14:40:19 +0900

    Add Views and split win32 and darwin

--- index.js
+++ index.js
@@ -2,26 +2,81 @@ var app = require('express')();
 const path = require('path');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-const redis = require("redis");
-const client = redis.createClient();
+const Redis = require("ioredis");
+const redis = new Redis();
+var moment = require('moment');
 var logging = [];
+var resultChunk = {
+  'total': 0,
+  'current_win': 0,
+  'current_darwin': 0,
+  'installed_win': 0,
+  'installed_darwin': 0,
+  'real_total' : 0,
+  'old_unique': 0,
+  'dayList': [],
+  'logList': logging,
+  'updated': new Date()
+};
 
-client.on("error", function(error) {
-  console.error(error);
-});
+const updater = {
+  common: () => {
+    resultChunk.logList = logging;
 
+    const pipe = redis.pipeline();
+    pipe.get("connected_win32_count"); // 0
+    pipe.get("connected_darwin_count");
+    pipe.scard("win32_unique_install");
+    pipe.scard("darwin_unique_install");
+    pipe.scard("unique_install");
+    pipe.exec((_, results) => {
+      resultChunk.current_win = parseInt(results[0][1]);
+      resultChunk.current_darwin = parseInt(results[1][1]);
+      resultChunk.installed_win = parseInt(results[2][1]);
+      resultChunk.installed_darwin = parseInt(results[3][1]);
+      resultChunk.old_unique = parseInt(results[4][1]);
+      resultChunk.total = parseInt(resultChunk.current_win) + parseInt(resultChunk.current_darwin);
+      resultChunk.updated = new Date();
+    });
+  },
+
+  updateDayListData: () => {
+    const dayList = [], now = moment();
+    const pipe = redis.pipeline();
+  
+    for(i of Array(60)) {
+      pipe.get(`date_${now.format("YYYYMMDD")}`);
+  
+      dayList.push({
+        day: now.format("YYYY-MM-DD"),
+        count: 0
+      });
+  
+      now.subtract(1, 'day');
+    }
+  
+    pipe.exec((_, results) => {
+      results.forEach((res, idx) => {
+        dayList[idx].count = parseInt(res[1]);
+      });
+  
+      resultChunk.dayList = dayList;
+    });
+  }
+}
+
+app.set('view engine', 'pug')
 app.get('/', (req, res) => {
-  var logHtml = '';
-
-  logging.forEach((v) => {
-    logHtml += `<li>${v}</li>`;
-  })
-
-  const currentCounter = Object.keys(io.sockets.connected).length;
-  client.SCARD('unique_install', (err, x) => {
-    res.send(`<html><head><title>CC</title></head><h1>NOW : ${currentCounter} / INSTALLED(U) : ${x}</h1><ul>${logHtml}</ul></html>`);
-  })
+  resultChunk.real_total =  Object.keys(io.sockets.connected).length;
+  res.render('index', resultChunk)
 });
+
+function addLog(msg) {
+  logging.unshift(`<span class="time">${(new Date()).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})}</span>: ${msg}`);
+  if (logging.length > 1000) {
+    logging.pop();
+  }
+}
 
 app.all("*", (req, res) => {
     res.sendStatus(404);
@@ -29,44 +84,56 @@ app.all("*", (req, res) => {
 
 io.on('connect', (soc) => {
     soc['uid'] = "unknown";
+
     soc.on('start', (data) => {
       try {
         soc['uid'] = data.uid;
+        soc['platform'] = data.platform;
       }catch(e){}
 
-      if (data.machine_id){
-        client.SADD("unique_install", data.machine_id)
+      if (data.platform === 'win32') {
+        redis.incr("connected_win32_count");
+        if (data.machine_id){
+          redis.sadd("win32_unique_install", data.machine_id)
+        }
+      } else if (data.platform === 'darwin') {
+        redis.incr("connected_darwin_count");
+        if (data.machine_id){
+          redis.sadd("darwin_unique_install", data.machine_id)
+        }
       }
 
-      logging.unshift(`${(new Date()).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})}: <b>[----------]</b> ${JSON.stringify(data)}`);
+      redis.incr("date_" + moment().format("YYYYMMDD"));
 
-      if (logging.length > 1000) {
-        logging.pop();
-      }
+      addLog(`<b>[----------]</b> ${JSON.stringify(data)}`)
     });
 
     soc.on('disconnect', () => {
-        logging.unshift(`${(new Date()).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})}: <b>[${soc['uid']}]</b> DISCONNECTED`);
-        if (logging.length > 1000) {
-          logging.pop();
+        addLog(`<b>[${soc['uid']}]</b> DISCONNECTED`)
+
+        if (soc.platform) {
+          if (soc.platform === 'darwin') {
+            redis.decr('connected_darwin_count');
+          } else if (soc.platform === 'win32') {
+            redis.decr('connected_win32_count');
+          }
         }
     });
 
     soc.on('fcm-error', (data) => {
-      logging.unshift(`${(new Date()).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})}: <b>[${soc['uid']}]</b> FCM ERROR <p style="color:red">${data}</p>`);
-      if (logging.length > 1000) {
-        logging.pop();
-      }
+      addLog(`<b>[${soc['uid']}]</b> FCM ERROR <p class="error">${data}</p>`);
     })
 
     soc.on('fcm-token', (data) => {
-      logging.unshift(`${(new Date()).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})}: <b>[${soc['uid']}]</b> <span style="color:blue">OK: ${data}</span>`);
-      if (logging.length > 1000) {
-        logging.pop();
-      }
+      addLog(`<b>[${soc['uid']}]</b> <span class="ok">OK: ${data}</span>`);
     })
 });
 
 http.listen(8080, () => {
   console.log('listening on *:8080');
 });
+
+setTimeout(() => {
+  updater.common();
+  updater.updateDayListData();
+}, 10000)
