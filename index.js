bf27e3ea63360dab9c7f89daa129f2c64083924e
KyeSeong Ahn <devflow@shelter.id>
2021-09-03 12:06:55 +0900

    Add Unique today connection and format index code.

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-var app = require('express')();
-const path = require('path');
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+const path = require("path");
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 const Redis = require("ioredis");
 const redis = new Redis();
-var moment = require('moment');
+var moment = require("moment");
 var logging = [];
 var resultChunk = {
-  'total': 0,
-  'current_win': 0,
-  'current_darwin': 0,
-  'installed_win': 0,
-  'installed_darwin': 0,
-  'real_total' : 0,
-  'old_unique': 0,
-  'dayList': [],
-  'logList': logging,
-  'updated': new Date()
+  total: 0,
+  current_win: 0,
+  current_darwin: 0,
+  installed_win: 0,
+  installed_darwin: 0,
+  real_total: 0,
+  old_unique: 0,
+  dayList: [],
+  logList: logging,
+  updated: new Date(),
 };
 
-redis.set('connected_win32_count', 0)
-redis.set('connected_darwin_count', 0)
+redis.set("connected_win32_count", 0);
+redis.set("connected_darwin_count", 0);
 
 const updater = {
   common: () => {
@@ -38,105 +38,116 @@ const updater = {
       resultChunk.installed_win = parseInt(results[2][1]);
       resultChunk.installed_darwin = parseInt(results[3][1]);
       resultChunk.old_unique = parseInt(results[4][1]);
-      resultChunk.total = parseInt(resultChunk.current_win) + parseInt(resultChunk.current_darwin);
+      resultChunk.total =
+        parseInt(resultChunk.current_win) +
+        parseInt(resultChunk.current_darwin);
       resultChunk.updated = new Date();
     });
   },
 
   updateDayListData: () => {
-    const dayList = [], now = moment();
+    const dayList = [],
+      now = moment();
     const pipe = redis.pipeline();
-  
-    for(i of Array(60)) {
+
+    for (i of Array(60)) {
       pipe.get(`date_${now.format("YYYYMMDD")}`);
-  
+      pipe.scard(`date_${now.format("YYYYMMDD")}_unique`);
+
       dayList.push({
         day: now.format("YYYY-MM-DD"),
-        count: 0
+        count: 0,
+        unique: 0,
       });
-  
-      now.subtract(1, 'day');
+
+      now.subtract(1, "day");
     }
-  
+
     pipe.exec((_, results) => {
       results.forEach((res, idx) => {
         dayList[idx].count = parseInt(res[1]);
+        dayList[idx].unique = parseInt(res[2]);
       });
-  
+
       resultChunk.dayList = dayList;
     });
-  }
-}
+  },
+};
 
-app.set('view engine', 'pug')
-app.get('/', (req, res) => {
-  resultChunk.real_total =  Object.keys(io.sockets.connected).length;
-  res.render('index', resultChunk)
+app.set("view engine", "pug");
+app.get("/", (req, res) => {
+  resultChunk.real_total = Object.keys(io.sockets.connected).length;
+  res.render("index", resultChunk);
 });
 
 function addLog(msg) {
-  logging.unshift(`<span class="time">${(new Date()).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})}</span>: ${msg}`);
+  logging.unshift(
+    `<span class="time">${new Date().toLocaleString("ko-KR", {
+      timeZone: "Asia/Seoul",
+    })}</span>: ${msg}`
+  );
   if (logging.length > 1000) {
     logging.pop();
   }
 }
 
 app.all("*", (req, res) => {
-    res.sendStatus(404);
-})
+  res.sendStatus(404);
+});
 
-io.on('connect', (soc) => {
-    soc['uid'] = "unknown";
+io.on("connect", (soc) => {
+  soc["uid"] = "unknown";
 
-    soc.on('start', (data) => {
-      try {
-        soc['uid'] = data.uid;
-        soc['platform'] = data.platform;
-      }catch(e){}
+  soc.on("start", (data) => {
+    try {
+      soc["uid"] = data.uid;
+      soc["platform"] = data.platform;
+    } catch (e) {}
 
-      if (data.platform === 'win32') {
-        redis.incr("connected_win32_count");
-        if (data.machine_id){
-          redis.sadd("win32_unique_install", data.machine_id)
-        }
-      } else if (data.platform === 'darwin') {
-        redis.incr("connected_darwin_count");
-        if (data.machine_id){
-          redis.sadd("darwin_unique_install", data.machine_id)
-        }
+    if (data.platform === "win32") {
+      redis.incr("connected_win32_count");
+      if (data.machine_id) {
+        redis.sadd("win32_unique_install", data.machine_id);
       }
+    } else if (data.platform === "darwin") {
+      redis.incr("connected_darwin_count");
+      if (data.machine_id) {
+        redis.sadd("darwin_unique_install", data.machine_id);
+      }
+    }
 
-      redis.incr("date_" + moment().format("YYYYMMDD"));
+    redis.incr("date_" + moment().format("YYYYMMDD"));
+    redis.sadd("date_" + moment().format("YYYYMMDD") + "_unique", data.uid);
 
-      addLog(`<b>[----------]</b> ${JSON.stringify(data)}`)
-    });
+    addLog(`<b>[----------]</b> ${JSON.stringify(data)}`);
+  });
 
-    soc.on('disconnect', () => {
-        addLog(`<b>[${soc['uid']}]</b> DISCONNECTED`)
+  soc.on("disconnect", () => {
+    addLog(`<b>[${soc["uid"]}]</b> DISCONNECTED`);
 
-        if (soc.platform) {
-          if (soc.platform === 'darwin') {
-            redis.decr('connected_darwin_count');
-          } else if (soc.platform === 'win32') {
-            redis.decr('connected_win32_count');
-          }
-        }
-    });
+    if (soc.platform) {
+      if (soc.platform === "darwin") {
+        redis.decr("connected_darwin_count");
+      } else if (soc.platform === "win32") {
+        redis.decr("connected_win32_count");
+      }
+    }
+  });
 
-    soc.on('fcm-error', (data) => {
-      addLog(`<b>[${soc['uid']}]</b> FCM ERROR <p class="error">${data}</p>`);
-    })
+  soc.on("fcm-error", (data) => {
+    addLog(`<b>[${soc["uid"]}]</b> FCM ERROR <p class="error">${data}</p>`);
+  });
 
-    soc.on('fcm-token', (data) => {
-      addLog(`<b>[${soc['uid']}]</b> <span class="ok">OK: ${data}</span>`);
-    })
+  soc.on("fcm-token", (data) => {
+    addLog(`<b>[${soc["uid"]}]</b> <span class="ok">OK: ${data}</span>`);
+  });
 });
 
 http.listen(8080, () => {
-  console.log('listening on *:8080');
+  console.log("listening on *:8080");
 });
 
 setInterval(() => {
   updater.common();
   updater.updateDayListData();
-}, 10000)
+}, 10000);
